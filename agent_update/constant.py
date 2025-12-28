177dcda2308e646c2c7308bc4f9fb4cd1c4b7479
Guo Xuanxuan <140936194+Fffflark@users.noreply.github.com>
2025-12-28 14:59:26 +0800

    Update constant.py

--- agent_update/constant.py
+++ agent_update/constant.py
@@ -4,5 +4,5 @@ rag_model_name: str = 'Qwen/Qwen3-8B'
 embedding_model_name: str = "NeuML/pubmedbert-base-embeddings"
 reranker_model_name: str = "cross-encoder/ms-marco-MiniLM-L-6-v2"
 similarity: int = 5
-user_role: str = "暧昧"
+user_role: str = "朋友"
 url: str = "http://localhost:6333"
